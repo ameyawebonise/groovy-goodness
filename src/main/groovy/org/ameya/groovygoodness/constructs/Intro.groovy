a6957e41511cd5b402839e42d6c0f627eb0ebab5
Ameya <Webonise@Ameyas-MacBook-Pro.local>
2015-09-29 18:05:15 +0530

    Changes for multiple assignments

--- src/main/groovy/org/ameya/groovygoodness/constructs/Intro.groovy
+++ src/main/groovy/org/ameya/groovygoodness/constructs/Intro.groovy
@@ -6,4 +6,21 @@ package org.ameya.groovygoodness.constructs
 class Intro {
 
     def x = "1"
+
+    def demonstrateMutipleAssigment (){
+        def (a, b, c) = [10, 20, "String"]
+    }
+
+    def demonstrateUnderflow(){
+        def (a, b,c) = [10, 20]
+        assert  a == 10 && b == 20 && c == null
+    }
+
+    def demonstrateOverflow(){
+        def (a, b) = [10, 20,30]
+        assert  a == 10 && b == 20
+    }
+
+
+
 }
--- src/test/groovy/org/ameya/groovygoodness/constructs/IntroSpec.groovy
+++ src/test/groovy/org/ameya/groovygoodness/constructs/IntroSpec.groovy
@@ -1,5 +1,6 @@
 package org.ameya.groovygoodness.constructs
 
+import spock.lang.Shared
 import spock.lang.Specification
 import org.ameya.groovygoodness.constructs.Intro;
 
@@ -8,10 +9,35 @@ import org.ameya.groovygoodness.constructs.Intro;
  */
 class IntroSpec extends  Specification {
 
+    @Shared
+    Intro intro = new Intro()
+
     def "variables definitions should be done properly"(){
-        setup:
-        Intro intro = new Intro()
         expect :
         assert intro.x == "1" : "Variable not correctly defined"
+    }
+
+    def "demonstrateMutipleAssigment should return the correct String"(){
+        when :
+        intro.demonstrateMutipleAssigment()
+        then :
+        assert  true
+    }
+
+    def "demonstrateUnderflow should  work as expected"(){
+        when :
+        intro.demonstrateUnderflow()
+        then :
+        assert  true
+
+    }
+
+
+    def "demonstrateOverflow should  work as expected"(){
+        when :
+        intro.demonstrateOverflow()
+        then :
+        assert  true
+
     }
 }
