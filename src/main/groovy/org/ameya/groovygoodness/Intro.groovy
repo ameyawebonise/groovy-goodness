f04eeee79c7700bc8b341709e0af7a1ea9020649
Ameya <Webonise@Ameyas-MacBook-Pro.local>
2015-09-29 16:49:15 +0530

    ADded the src folder

---
+++ src/main/groovy/org/ameya/groovygoodness/Intro.groovy
@@ -0,0 +1,7 @@
+package groovy.org.ameya.groovygoodness
+
+/**
+ * Created by Webonise on 29/09/15.
+ */
+class Intro {
+}
---
+++ src/test/groovy/org/ameya/groovygoodness/IntroSpec.groovy
@@ -0,0 +1,9 @@
+package groovy.org.ameya.groovygoodness
+
+import spock.lang.Specification
+
+/**
+ * Created by Webonise on 29/09/15.
+ */
+class IntroSpec extends  Specification {
+}
